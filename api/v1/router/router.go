3fdf4c4a3970eaf1f6e4027d19bf557936281cd9
Lawrence Wong <lawrencedrums@gmail.com>
2023-09-28 12:50:19 +0100

    feat: introduce TailwindCSS

--- api/v1/router/router.go
+++ api/v1/router/router.go
@@ -1,9 +1,10 @@
 package router
 
 import (
-    "github.com/gorilla/mux"
+	"net/http"
 
-    "github.com/lawrencedrums/gojira/api/v1/handlers"
+	"github.com/gorilla/mux"
+	"github.com/lawrencedrums/gojira/api/v1/handlers"
 )
 
 func NewRouter() *mux.Router {
@@ -17,6 +18,9 @@ func NewRouter() *mux.Router {
     router.HandleFunc("/issues/edit/{id}", handlers.EditIssue).Methods("GET")
     router.HandleFunc("/issues/new/", handlers.NewIssueForm).Methods("GET")
     router.HandleFunc("/reset", handlers.Reset).Methods("GET")
+
+    staticDir := "./cmd/gojira/static"
+    router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 
     return router
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./cmd/gojira/templates/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
